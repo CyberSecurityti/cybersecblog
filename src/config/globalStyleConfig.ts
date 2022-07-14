748c79c040fa3c4138db204c7b0ec5f4753223e9
higordevv <pedrohigor2004@gmail.com>
2022-07-14 15:19:02 -0300

    modified:   index.html 	new file:   package-lock.json 	modified:   package.json 	deleted:    src/App.css 	modified:   src/App.tsx 	Adicionando Paleta de cores 	Adicionando Style Global 	deleted:    src/favicon.svg 	deleted:    src/index.css 	deleted:    src/logo.svg 	modified:   src/main.tsx

---
+++ src/config/globalStyleConfig.ts
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from 'styled-components';
+import { primaryRedColor, primary_blackColor } from './colorPalette';
+
+
+export default createGlobalStyle`
+
+    * {
+        margin: 0%;
+        padding: 0%;
+        outline: none;
+        box-sizing: border-box;
+        color: white;
+    }
+
+    body {
+        font-family: sans-serif;
+        background-color: ${primary_blackColor};
+    }
+
+    html, body, #root {
+        height: 100%;
+    }
+
+    button {
+        cursor: pointer;
+        background-color: ${primaryRedColor};
+        border: none;
+        padding: 10px 20px;
+        border-radius: 4px;
+        font-weight: 700;
+    }
+
+    a {
+        text-decoration: none;
+        color: ${primaryRedColor};
+    }
+
+    ul {
+        list-style: none;
+    }
+
+
+
+`
+
