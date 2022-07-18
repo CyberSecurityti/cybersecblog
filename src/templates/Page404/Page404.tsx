72e774b32d063765d0bd5d50a094922fa30eb968
higordevv <pedrohigor2004@gmail.com>
2022-07-18 17:21:14 -0300

    Page 404 + Prank

--- src/templates/Page404/Page404.tsx
+++ src/templates/Page404/Page404.tsx
@@ -1,9 +1,31 @@
-import Page404div from './style/style'
+import { Page404div, TextArea } from "./style/style";
+import { Link, Navigate} from "react-router-dom";
+import {Howl,Howler} from 'howler'
 
-export const Page404 = () => {
-    return (
-        <Page404div>
-            <h1>ERROR 404 - Pagina não encontrada</h1>
-        </Page404div>
-    )
+const Prankkkk = () => {
+   const sound = new Howl({
+      src: 'https://memes.casa/audios/gemido-whatsapp.mp3',
+      autoplay: true,
+      loop:false,
+      volume: 0.5,
+   })
 }
+export const Page404 = () => {
+   return (
+      <Page404div>
+         <div className="img">
+            <img src="https://assets.codepen.io/5647096/backToTheHomepage.png" />
+            <img src="https://assets.codepen.io/5647096/Delorean.png" />
+         </div>
+         <TextArea>
+            <h1>404</h1>
+            <h2>Página não encontrada</h2>
+            <h3>Voltar pra Home?</h3>
+            <Link to={'/'} className="Link">
+               Sim
+            </Link>
+            <a onClick={Prankkkk} >Não</a>
+         </TextArea>
+      </Page404div>
+   );
+};
--- src/templates/Page404/style/style.tsx
+++ src/templates/Page404/style/style.tsx
@@ -1,12 +1,58 @@
-import styled from 'styled-components';
-
+import styled from "styled-components";
 
 const Page404div = styled.div`
-    display: flex;
-    width: 100%;
-    height: 80vh;
-    justify-content: center;
-    align-items: center;
-`
+   display: flex;
+   width: 100%;
+   height: 100%;
+   justify-content: center;
+   align-items: center;
+   font-family: "Press Start 2P", cursive;
+   h1 {
+      color: red;
+      font-size: 100px;
+   }
+   h2 {
+      font-size: 50px;
+   }
+   h1,
+   h2,
+   h3 {
+      margin-bottom: 40px;
+   }
+   @media only screen and (max-width: 768px) {
+      flex-direction: column;
+      justify-content: center;
+      img{
+         width: 80vw;
+         height: auto;
+      }
+      h1 {
+         font-size: 50px;
+      }
+      h2 {
+         font-size: 25px;
+      }
+   }
+`;
+const TextArea = styled.div`
+   height: 50vh;
+   .Link {
+      text-decoration: none;
+      margin-right: 20px;
+   }
+   .Link:hover {
+      color: red;
+      text-decoration: underline;
+   }
+   @media only screen and (max-width: 768px) {
+      width: 100%;
+      margin: 25px auto;
+      text-align: center;
+      a:active {
+         color: red;
+         text-decoration: underline;
+      }
+   }
+`;
 
-export default Page404div
+export { Page404div, TextArea };
