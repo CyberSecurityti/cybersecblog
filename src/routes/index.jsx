b088c09c59dc21bf6e7083352ff00025bd4ca482
higordevv <pedrohigor2004@gmail.com>
2022-07-19 12:41:18 -0300

    Corrigindo RouterConfig 	renamed:    src/App.tsx -> src/App.jsx 	renamed:    src/components/header/styles/stylesnav.ts -> src/components/header/styles/stylesnav.js 	renamed:    src/config/colorPalette.ts -> src/config/colorPalette.js 	renamed:    src/config/globalStyleConfig.ts -> src/config/globalStyleConfig.js 	renamed:    src/routes/index.tsx -> src/routes/index.jsx 	renamed:    src/templates/Page404/Page404.tsx -> src/templates/Page404/Page404.jsx 	renamed:    src/templates/Page404/style/style.tsx -> src/templates/Page404/style/style.jsx 	renamed:    src/templates/courses/courses.tsx -> src/templates/courses/courses.jsx 	renamed:    src/templates/courses/styles/styles.tsx -> src/templates/courses/styles/styles.jsx

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,7 +6,7 @@ import { Page404 } from "../templates/Page404/Page404";
 function RouterConfig() {
   return (
     <Routes>
-        <Route index element={<App />} />
+        <Route index path='/' element={<App />} />
         <Route path="/courses" element={<Courses />} />
         <Route path="*" element={<Page404 />} />
     </Routes>
